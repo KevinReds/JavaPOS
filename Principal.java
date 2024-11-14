4feecec3712941be95bab465d43b170e0e82eae6
KevinReds <98777689+KevinReds@users.noreply.github.com>
2024-11-14 17:05:55 -0500

    principal

--- Principal.java
+++ Principal.java
@@ -81,16 +81,102 @@ public class Principal extends JFrame implements ActionListener {
     mb.add (menuColorFondo);
 
     miCalculo  = new JMenuItem("Vacaciones");
-
-    miCalculo.setFont (
-
-    new Font("Andale Mono", 1, 14));
-    miCalculo.setForeground (
-
-    new Color(255, 0, 0));
+    miCalculo.setFont (new Font("Andale Mono", 1, 14));
+    miCalculo.setForeground (new Color(255, 0, 0));
     menuCalcular.add (miCalculo);
+    miCalculo.addActionListener (this);
 
-    miCalculo.addActionListener (
+    miRojo  = new JMenuItem("Vacaciones");
+    miRojo.setFont (
+    new Font("Andale Mono", 1, 14));
+    miRojo.setForeground (new Color(255, 0, 0));
+    menuColorFondo.add (miRojo);
+    miRojo.addActionListener (this);
 
-this);
+    miNegro  = new JMenuItem("Vacaciones");
+    miNegro.setFont (
+    new Font("Andale Mono", 1, 14));
+    miNegro.setForeground (new Color(0, 0, 0));
+    menuColorFondo.add (miNegro);
+    miNegro.addActionListener (this);
+
+    miMorado  = new JMenuItem("Vacaciones");
+    miMorado.setFont (new Font("Andale Mono", 1, 14));
+    miMorado.setForeground (new Color(148, 0, 211));
+    menuColorFondo.add (miMorado);
+    miMorado.addActionListener (this);
+
+    miNuevo= new JMenuItem("Nuevo");
+    miNuevo.setFont (new Font("Andale Mono", 1, 14));
+    miNuevo.setForeground (new Color(255, 0, 0));
+    menuOpciones.add (miNuevo);
+    miNuevo.addActionListener (this);
+
+    miSalir= new JMenuItem("Salir");
+    miSalir.setFont (new Font("Andale Mono", 1, 14));
+    miSalir.setForeground (new Color(255, 0, 0));
+    menuOpciones.add (miSalir);
+    miSalir.addActionListener (this);
+
+    miElCreador= new JMenuItem("El Creador");
+    miElCreador.setFont (new Font("Andale Mono", 1, 14));
+    miElCreador.setForeground (new Color(255, 0, 0));
+    menuAcercaDe.add (miElCreador);
+    miElCreador.addActionListener (this);
+
+    imageIcon imagen = new ImageIcon("imagenes/iconPequeño.jpg");
+    labelLogo=new JLabel(imagen);
+    labelLogo.setBounds(5,5,250,100);
+    add(labelLogo);
+
+    labelBienvenido = new JLabel("Bienvenido");
+    labelBienvenido.setBounds(280,30,300,50);
+    labelBienvenido.setFont (new Font("Andale Mono", 1, 32));
+    labelBienvenido.setForeground (new Color(255, 255, 255));
+    add(labelBienvenido);
+
+    labelTitle = new JLabel("Datos del trabajador para calculo de vacaciones");
+    labelTitle.setBounds(45,140,900,25);
+    labelTitle.setFont (new Font("Andale Mono", 1, 24));
+    labelTitle.setForeground (new Color(255, 255, 255));
+    add(labelTitle);
+
+    labelNombre = new JLabel("Nombre completo");
+    labelNombre.setBounds(25, 188, 180, 25);
+    labelNombre.setFont(new Font("Andale Mono", 1, 12));
+    labelNombre.setForeground(new Color(255,255,255));
+    add(labelNombre);
+
+    txtNombreTrabajador= new JTextField();//textfields?
+    txtNombreTrabajador.setBounds(25, 213, 150, 25);
+    txtNombreTrabajador.setBackground(new java.awt.Color(224,224,224));
+    txtNombreTrabajador.setFont(new Font("Andale Mono", 1, 14));
+    txtNombreTrabajador.setForeground(new java.awt.Color(225,0,0));
+    add(txtNombreTrabajador);
+
+    labelAPaterno = new JLabel("Apellido paterno");
+    labelAPaterno.setBounds(25, 248, 180, 25);
+    labelAPaterno.setFont(new Font("Andale Mono", 1, 12));
+    labelAPaterno.setForeground(new Color(255,255,255));
+    add(labelAPaterno);
+
+    txtAPaternoTrabajador= new JTextField();//textfields?
+    txtAPaternoTrabajador.setBounds(25, 273, 150, 25);
+    txtAPaternoTrabajador.setBackground(new java.awt.Color(224,224,224));
+    txtAPaternoTrabajador.setFont(new Font("Andale Mono", 1, 14));
+    txtAPaternoTrabajador.setForeground(new java.awt.Color(225,0,0));
+    add(txtAPaternoTrabajador);
+
+    labelAMaterno = new JLabel("Apellido paterno");
+    labelAMaterno.setBounds(25, 248, 180, 25);
+    labelAMaterno.setFont(new Font("Andale Mono", 1, 12));
+    labelAMaterno.setForeground(new Color(255,255,255));
+    add(labelAMaterno);
+
+    txtAMaternoTrabajador= new JTextField();//textfields?
+    txtAMaternoTrabajador.setBounds(25, 273, 150, 25);
+    txtAMaternoTrabajador.setBackground(new java.awt.Color(224,224,224));
+    txtAMaternoTrabajador.setFont(new Font("Andale Mono", 1, 14));
+    txtAMaternoTrabajador.setForeground(new java.awt.Color(225,0,0));
+    add(txtAMaternoTrabajador);
 }
