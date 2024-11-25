8f1e863844f4983f538c41db3c39a195ee187d00
KevinReds <98777689+KevinReds@users.noreply.github.com>
2024-11-25 08:54:48 -0500

    Projecto Netbeans MalliePOS

---
+++ MalliePOS/src/main/java/com/sena/malliepos/MalliePOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sena.malliepos;
+
+/**
+ *
+ * @author Kevin
+ */
+public class MalliePOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
