6a91ef47315901d3187492d45f31aaf3d6f7f681
SantiSerranoXD27 <santicubides2006@gmail.com>
2024-11-26 23:54:12 -0500

    interfaces conectadas

---
+++ MalliePOS/src/com/sena/malliepos/MalliePOS.java
@@ -0,0 +1,25 @@
+package com.sena.malliepos;
+
+import GUI.Bienvenida;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author santi
+ */
+public class MalliePOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bienvenida ventana = new Bienvenida();
+        ventana.setVisible(true);
+    }
+    
+}
